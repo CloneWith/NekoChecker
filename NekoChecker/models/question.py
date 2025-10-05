e5cea01d82b95cdd901b61e15178af4b48692bee
CloneWith <clonewith@qq.com>
2025-10-05 14:36:34 +0800

    Mark some properties of questions as optional

--- NekoChecker/models/question.py
+++ NekoChecker/models/question.py
@@ -1,18 +1,19 @@
+import re
+
 from pydantic import BaseModel
-from typing import Optional
 
 
 class Question(BaseModel):
     id: int = 0
-    description: str = ""
-    format: str = ""
-    format_re: str = ""
-    answer: str = ""
+    description: str
+    format: str | None = None
+    format_re: str | None = None
+    answer: str
     tries_count: int = 0
     max_tries: int = -1
     solved: bool = False
     last_wrong_times: list[float] = []  # 记录错误时间戳
-    banned_until: Optional[float] = None  # 被禁止答题的截止时间
+    banned_until: float | None = None  # 被禁止答题的截止时间
 
     def record_wrong(self):
         import time
@@ -34,5 +35,9 @@ class Question(BaseModel):
         return time.time() < self.banned_until
 
     def check_pattern(self, flag: str):
-        import re
-        re.compile(flag)
+        # No explicit requirement for flag pattern
+        if self.format_re is None:
+            return True
+
+        check_re = re.compile(self.format_re)
+        return check_re.match(flag)
